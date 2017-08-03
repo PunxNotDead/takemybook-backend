a9445cf4e1d31af2b6c1b25e2e67d98b4238b66e
Mikhail Ziankevich <zin@zame-dev.org>
2017-08-03 23:35:45 +0200

    Google login

--- src/api_router.js
+++ src/api_router.js
@@ -1,7 +1,81 @@
 const router = require('express').Router();
 
-
 const user = require('./controllers/user_controller');
+
+
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+function extractProfile (profile) {
+  let imageUrl = '';
+  if (profile.photos && profile.photos.length) {
+    imageUrl = profile.photos[0].value;
+  }
+  return {
+    id: profile.id,
+    displayName: profile.displayName,
+    image: imageUrl
+  };
+}
+
+// Configure the Google strategy for use by Passport.js.
+//
+// OAuth 2-based strategies require a `verify` function which receives the
+// credential (`accessToken`) for accessing the Google API on the user's behalf,
+// along with the user's profile. The function must invoke `cb` with a user
+// object, which will be set at `req.user` in route handlers after
+// authentication.
+passport.use(new GoogleStrategy({
+  clientID: '663608028282-qngrticdc4d8k24sbejp96l49bgde2uf.apps.googleusercontent.com',
+  clientSecret: '1uClQlEVzifEv3ESA-Ww1_U1',
+  callbackURL: '/api/google/callback',
+  accessType: 'offline'
+}, (accessToken, refreshToken, profile, cb) => {
+  // Extract the minimal profile information we need from the profile object
+  // provided by Google
+  cb(null, extractProfile(profile));
+}));
+
+passport.serializeUser((user, cb) => {
+  cb(null, user);
+});
+passport.deserializeUser((obj, cb) => {
+  cb(null, obj);
+});
+
+router.get(
+  // Login url
+  '/auth/login',
+
+  // Save the url of the user's current page so the app can redirect back to
+  // it after authorization
+  (req, res, next) => {
+    if (req.query.return) {
+      req.session.oauth2return = req.query.return;
+    }
+    next();
+  },
+
+  // Start OAuth 2 flow using Passport.js
+  passport.authenticate('google', { scope: ['email', 'profile'] })
+);
+
+router.get(
+  // OAuth 2 callback url. Use this url to configure your OAuth client in the
+  // Google Developers console
+  '/google/callback',
+
+  // Finish OAuth 2 flow using Passport.js
+  passport.authenticate('google'),
+
+  // Redirect back to the original page, if any
+  (req, res) => {
+    const redirect = req.session.oauth2return || '/';
+    delete req.session.oauth2return;
+    res.redirect(redirect);
+  }
+);
+
 
 router.get('/users/list', user.list);
 
